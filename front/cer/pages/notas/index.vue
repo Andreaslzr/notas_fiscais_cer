a799b9c7e62f8e5c29b8a1235387f66421e37bae
Desenvolvimento <dsadm@educ501.local>
2024-06-11 22:40:09 -0300

    inicio

---
+++ front/cer/pages/notas/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+definePageMeta({
+    middleware:'auth'
+})
+
+
+</script>
+
+<template>
+    <main class="notas-container flex flex-column align-items-center">
+    <h1>Buscar notas fiscais</h1>
+    <section>
+            <div>
+                <FloatLabel>
+                    <InputText/>
+                    <label>Buscar pelo número da nota</label>
+                </FloatLabel> <br>
+                <FloatLabel>
+                    <InputText/>
+                    <label>Buscar pela data de início</label>
+                </FloatLabel> <br>
+                <FloatLabel>
+                    <InputText/>
+                    <label>Buscar pela data de fim</label>
+                </FloatLabel> <br>
+                
+            </div>
+        </section>
+    </main>
+</template>
+
+<style scoped lang="scss">
+
+.notas-container {
+  margin: 0;
+  width: 100vw;
+  min-height: calc(100vh - 80px);
+  background-color: rgb(154, 154, 243);
+  background-repeat: repeat;
+  background-size: cover;
+}
+
+</style>
